9ddb06133a06af844f63974b1be0ee7c2c83acb4
fernanda-2 <80990129+fernanda-2@users.noreply.github.com>
2021-03-19 18:52:08 -0600

    Update and rename mi-footer.js to primos.js

--- cmp/primos.js
+++ cmp/primos.js
@@ -1,7 +1,7 @@
-class MiFooter extends HTMLElement {
+class Primos extends HTMLElement {
   connectedCallback() {
     this.innerHTML = /*html*/
       `Copyright &copy; 2021 González Vargas Laura Fernanda.`;
   }
 }
-customElements.define("mi-footer", MiFooter);
+customElements.define("primos", Primos);
